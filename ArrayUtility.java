72b5ffecdfc0ebe04ecaa017942d18ea01226918
SAURABH KUMAR <105958643+saurabh113v@users.noreply.github.com>
2025-06-10 10:47:33 +0530

    Create ArrayUtility.java

---
+++ ArrayUtility.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static int[] inputArray(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number of element: ");
+        int size = input.nextInt();
+        int[] nums = new int[size];
+        int i= 0;
+        while (i<size) {
+            System.out.println("Please Enter element number " + (i+1)+ ":");
+            nums[i] = input.nextInt();
+            i++;   
+        }
+       return nums;
+    }
+}
